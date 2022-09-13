5834bf3ae9afd72b066d0126842c9bfe8f78bc69
ravishukla21 <112763085+ravishukla21@users.noreply.github.com>
2022-09-13 13:26:42 +0530

    Add files via upload

---
+++ 3.js
@@ -0,0 +1,11 @@
+let a=3;
+let b=3;
+if(a>b){
+  console.log("a is greater than b")
+  
+}else if(b>a){
+  console.log("b is greater than a")
+  
+}else{
+  console.log("both are equal")
+}
---
+++ 4.js
@@ -0,0 +1,13 @@
+let realpass=123;
+let realid="qwerty";
+let inputpass=123;
+let inputid="qwety";
+if (inputid==realid){
+  if(inputpass==realpass){
+    console.log("Correct Password");
+  }else{
+    console.log("Incorrect Password");
+    }
+}else{
+  console.log("Incorrect id")
+}
